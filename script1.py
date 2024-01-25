3b998664eddd13202de425b6fba2a1a3ef2557b0
jachaudh <156561167+jachaudh@users.noreply.github.com>
2024-01-25 11:46:59 -0500

    Step 4 completed
    added function to print name and ID.

--- script1.py
+++ script1.py
@@ -22,6 +22,16 @@ def main():
     add_movie = {'title': 'The Godfather', 'genre': 'crime'}
     about_me['movies'].append(add_movie)
 
+   
+def print_student_name_and_id(about_me):
+
+    full_name = about_me['full_name']
+    student_id = about_me['student_id']
+    first_name = full_name.split()[0]
+
+    print(f"My name is {full_name}, but you can call me Sir {first_name}.")
+    print(f"My student ID is {student_id}.")
+
 
     return
 
