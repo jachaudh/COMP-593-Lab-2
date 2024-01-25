c862134c4cacdd77ab20ae862d085daef9b61880
jachaudh <156561167+jachaudh@users.noreply.github.com>
2024-01-25 11:42:23 -0500

    Step 2 completed
    added a dictionary

--- script1.py
+++ script1.py
@@ -1,5 +1,23 @@
 def main():
     
+    about_me = {
+
+        'full_name': 'Jay Chaudhari',
+        'student_id': 10291240  ,
+        'pizza_toppings': ['MUSHROOMS', 'ONION', 'BLACKPEPPER'],
+        'movies': [
+
+            {
+                'title': 'Intersteller',
+             'genre': 'Sci-Fi',
+            }
+            {
+            'title': 'FordvsFerrari', 
+             'genre': 'Drama',
+            }
+        ]
+    }
+
     return
 
 if __name__ == '__main__':
